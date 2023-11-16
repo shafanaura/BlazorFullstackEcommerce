7c13f13b4a595353fd82b3fef521b16df25b1fda
Shafa Naura Priesha Ardhynindya <shafanaura48@gmail.com>
2023-11-16 16:50:11 +0700

    first commit

---
+++ BlazorFullstackEcommerce/Client/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,razor}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('@tailwindcss/forms')],
+}
